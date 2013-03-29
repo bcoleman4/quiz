e8b1d2e376503a21a916337dd164ad72064f0ad5
Brian <bcoleman4@hotmail.co.uk>
2013-03-29 23:09:14 +0000

    Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,23 @@ class HomeController < ApplicationController
   def create
   end
   
-  def new
+  def do
+      if params[:exp] = '1' #remember that params come back as strings
+        do_something
+      elsif params[:exp] = '2'
+        do_something_else
+      else
+        handle_the_no_default_radio_button_case # if you want one to be checked, supply true as the third parameter
+      end
+
+      respond_to do |format|
+        format.js {
+      render :update do |page|
+        page.replace_html or some other reponse
+      end
+      }
+      end
+      format.html { redirect_to questions_url }
   end
   
   def index
